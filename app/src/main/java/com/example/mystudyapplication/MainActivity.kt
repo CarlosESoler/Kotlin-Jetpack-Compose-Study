8b0b8b04655c19ae8566490b32ab996afc09b8e6
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-12 18:24:47 -0300

    Criando um component para view de uma mensagem

--- app/src/main/java/com/example/mystudyapplication/MainActivity.kt
+++ app/src/main/java/com/example/mystudyapplication/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.mystudyapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mystudyapplication.ui.theme.MyStudyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +25,66 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyStudyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Carlos")
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(
+                        Message("Léia", "Ta demitido")
+                    )
                 }
             }
         }
     }
 }
 
+
+data class Message(val author: String, val body: String) // Model
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            // Author
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            // Message From Author
+            Surface(shape = MaterialTheme.shapes.medium) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)// Only dev mode
 @Composable
-fun GreetingPreview() {
-    MyStudyApplicationTheme {
-        Greeting("Carlos")
-    }
+fun PreviewMessageCard() {
+    MessageCard(
+        Message(
+            "Léia", "Foi decidido que você é o cara para assumir o projeto"
+        )
+    )
 }
