e4899fd62f7e6bf23488b60e63877f6764aea49d
Carlos Soler <carlinhoscaco09@gmail.com>
2023-10-12 20:16:17 -0300

    Add: Lista de mensagens, colocando animação para a mensagem ao clicar em cima

--- app/src/main/java/com/example/mystudyapplication/MainActivity.kt
+++ app/src/main/java/com/example/mystudyapplication/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.mystudyapplication
 
+import SampleData
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -25,9 +30,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyStudyApplicationTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(
-                        Message("Léia", "Ta demitido")
+                Conversation(
+                    SampleData.conversationSample
+                )
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String) // Model
+
+@Composable
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
+            modifier = Modifier.size(40.dp).clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            // Author
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                tonalElevation = 3.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                // Message From Author
+                Surface(shape = MaterialTheme.shapes.medium) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
                     )
                 }
             }
@@ -35,51 +87,20 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-data class Message(val author: String, val body: String) // Model
-
 @Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = null,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Column {
-            // Author
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.bodyLarge,
-            )
-
-            Spacer(modifier = Modifier.height(5.dp))
-
-            // Message From Author
-            Surface(shape = MaterialTheme.shapes.medium) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
+
 @Preview(name = "Light Mode")
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)// Only dev mode
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     MessageCard(
@@ -87,4 +108,13 @@ fun PreviewMessageCard() {
             "Léia", "Foi decidido que você é o cara para assumir o projeto"
         )
     )
+}
+
+// List of messages
+@Preview
+@Composable
+fun PreviewConversation() {
+    MyStudyApplicationTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
